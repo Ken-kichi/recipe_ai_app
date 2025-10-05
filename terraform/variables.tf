bb2a4dbd870555b3e0472651cebdff331ec4f785
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-10-05 14:17:18 +0900

    terraformの値を追加

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "container_app_cpu" {
+  type = number
+}
+
+variable "container_app_memory" {
+  type = string
+}
+
+variable "postgresql_login_name" {
+  type = string
+}
+
+variable "postgresql_password" {
+  type = string
+}
