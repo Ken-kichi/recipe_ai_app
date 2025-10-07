dffa68e537721dfaafe5ba24f5bad19637723027
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2025-10-07 21:50:24 +0900

    認証部分を仮実装

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from src.models import User, UserInDB
+from src.utils import get_current_active_user, fake_hash_password
+from src.database import fake_users_db
+from src.api.auth.index import router as auth_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
